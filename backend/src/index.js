c2fbaeafa444be316b30790b48846cbe971cdcbd
Pavan Kumar Choudari <pavankumarchoudari1@gmail.com>
2026-09-21 19:31:06 +0530

    INTIALIZING THE FRONTEND AND BACKEND OF THE PROJECT

---
+++ backend/src/index.js
@@ -0,0 +1,7 @@
+ // const  express = require('express');
+import express from 'express';
+import "dotenv/config";
+const app = express();
+const PORT = process.env.PORT
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
