f3b7caa468ce00b7b43ab4eadf2aaf15210d0f00
Pavan Kumar Choudari <pavankumarchoudari1@gmail.com>
2026-09-22 11:23:48 +0530

    .

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,30 @@
- // const  express = require('express');
 import express from 'express';
 import "dotenv/config";
-const app = express();
-const PORT = process.env.PORT
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+import { clerkMiddleware } from '@clerk/express'
+import cors from 'cors';
+
+import User from './models/user.model.js';
+
+import { connectDB } from "./lib/db.js";
+
+const app = express();
+
+const PORT = process.env.PORT;
+const FRONTEND_URL = process.env.FRONTEND_URL;
+
+app.use(express.json());
+app.use(cors({ origin: FRONTEND_URL, credentials: true));
+
+app.use(clerkMiddleware{});
+
+app.get("/health", (req, res) => {
+
+    
+    res.status(200).json({ ok: true });
+});
+
+app.listen(PORT, () => {
+    connectDB();
+    console.log(`Server is running on port ${PORT}`);
+});
---
+++ backend/src/lib/db.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+
+export async function connectDB() {
+    try {
+        const mongoUri = process.env.MONGO_URI;
+
+        if (!mongoUri) {
+            throw new Error("MONGO_URI is required");
+        }
+
+        const conn = await mongoose.connect(mongoUri);
+
+        console.log(`MongoDB connected: ${conn.connection.host}`);
+
+    } catch (error) {
+        console.error("Error connecting to MongoDB", error.message);
+        process.exit(1);
+    }
+}
