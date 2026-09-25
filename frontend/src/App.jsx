6fc8ffeb0d258a5e8384a4840f260e0f6d075798
Pavan Kumar Choudari <pavankumarchoudari1@gmail.com>
2026-09-25 15:12:32 +0530

    feat: integrate theming and wallpaper context, add routing and authentication pages
    - Added ThemeProvider and WallpaperProvider for managing themes and wallpapers.
    - Integrated react-router for navigation between ChatPage and AuthPage.
    - Updated App component to handle authentication state and routing.
    - Added new pages: AuthPage and ChatPage.
    - Included theme presets and styles for dynamic theming.
    - Added various wallpapers and sound assets for enhanced user experience.
    - Updated package.json to include necessary dependencies for theming and routing.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,26 @@
-import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/react'
-import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import { WallpaperProvider } from './context/WallpaperContext'
+import { ThemeProvider } from './context/ThemeContext'
+import ChatPage from './pages/ChatPage'
+import AuthPage from './pages/AuthPage'
+import { useAuth } from "@clerk/react"
 
 function App() {
+  const { isSignedIn, isLoaded} = useAuth();
+
+  if(!isLoaded) return <p>Loading...</p>;
+
 
   return (
-    <div>
-      <h1> MY APP</h1>
-      <header>
-        <Show when="signed-out">
-          <SignInButton mode = "modal" />
-          <SignUpButton mode = "modal"  />
-        </Show>
-
-        <Show when="signed-in">
-          <UserButton />
-        </Show>
-      </header>
-    </div>
+    <ThemeProvider>
+      <WallpaperProvider>
+        <Routes>
+          <Route path="/" element={ isSignedIn  ? <ChatPage /> : <Navigate to={"/auth"} replace  /> } />
+          <Route path="/auth" element={ !isSignedIn ? <AuthPage /> : <Navigate to={"/"} replace /> } />
+        </Routes>
+      </WallpaperProvider>
+    </ThemeProvider>
   )
-}
+}  
 
 export default App
